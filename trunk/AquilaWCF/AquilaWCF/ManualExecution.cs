1e42bb7f87e70ee2d58fcec8cdad927cc0a02160
Josef Sochovsky <josef.nikolaus@sochovsky.at>
2013-02-13 15:57:13 +0100

    Added Worker.cs and ManualExecution.cs

---
+++ trunk/AquilaWCF/AquilaWCF/ManualExecution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AquilaWCF
+{
+    enum ManualExecution
+    {
+        Accepted,
+        Denied,
+        Pending
+    }
+}
---
+++ trunk/AquilaWCF/AquilaWCF/Worker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Krs.Ats.IBNet.Contracts;
+using Krs.Ats.IBNet;
+
+namespace AquilaWCF
+{
+    class Worker
+    {
+        public volatile ManualExecution ManualExecution; 
+        public volatile Equity Equity;
+        public volatile bool IsActive;
+        public volatile decimal Amount;
+        public volatile BarSize BarSize;
+        public volatile HistoricalDataType historicalDataType;
+        public volatile RealTimeBarType RealtimeBarType;
+        public volatile bool _Running;
+        public volatile decimal pricePremium;
+    }
+}
