277ba5a844ae09778669dcaabf6b33b52705b42f
Josef Sochovsky <josef.nikolaus@sochovsky.at>
2013-02-13 16:12:35 +0100

    Changed Worker.cs

--- trunk/AquilaWCF/AquilaWCF/Worker.cs
+++ trunk/AquilaWCF/AquilaWCF/Worker.cs
@@ -5,18 +5,18 @@ using System.Text;
 using Krs.Ats.IBNet.Contracts;
 using Krs.Ats.IBNet;
 
-namespace AquilaWCF
+namespace Aquila_Software
 {
     class Worker
     {
         public volatile ManualExecution ManualExecution; 
         public volatile Equity Equity;
         public volatile bool IsActive;
-        public volatile decimal Amount;
+        public volatile float Amount;
         public volatile BarSize BarSize;
         public volatile HistoricalDataType historicalDataType;
         public volatile RealTimeBarType RealtimeBarType;
         public volatile bool _Running;
-        public volatile decimal pricePremium;
+        public volatile float PricePremiumPercentage;
     }
 }
