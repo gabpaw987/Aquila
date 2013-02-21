8836fe6c823285f944fb7c112448ac002d0f0340
gpawlowsky <gabriel_pawlowsky@yahoo.de>
2013-02-21 15:50:01 +0100

    Added WCFMessage

---
+++ trunk/AquilaWCF/AquilaWCF/WCFMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Aquila_Software
+{
+    [DataContract]
+    public class WCFMessage
+    {
+        [DataMember(IsRequired = true)]
+        public string args { get; set; }
+
+        public WCFMessage(string args)
+        {
+            this.args = args;
+        }
+    }
+}
