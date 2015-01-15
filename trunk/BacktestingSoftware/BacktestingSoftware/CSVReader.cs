72b6aabfed28a681d6edff1d07f7c237fd577e01
gpawlowsky <gabriel_pawlowsky@yahoo.de>
2015-01-14 18:58:16 -0800

    Corrected CSV reader to read Josefs exported files

--- trunk/BacktestingSoftware/BacktestingSoftware/CSVReader.cs
+++ trunk/BacktestingSoftware/BacktestingSoftware/CSVReader.cs
@@ -21,6 +21,7 @@ namespace BacktestingSoftware
         public static IEnumerable<Tuple<DateTime, decimal, decimal, decimal, decimal, long>> EnumerateExcelFile(string filePath, DateTime startDate, DateTime endDate, bool isFullFuturePriceData, int innerValue)
         {
             char splitter = ',';
+            bool isCommaDecimalPoint = false;
             //Indices: date, time, open, high, low, close, volume
             int[] indices = new int[7];
             for(int i = 0; i < indices.Length; i++)
@@ -41,7 +42,7 @@ namespace BacktestingSoftware
                 else if (header.Count(c => c == ';') > header.Count(c => c == ','))
                 {
                     splitter = ';';
-                }
+                }                
 
                 string[] headerValues = header.Split(splitter);
 
@@ -75,12 +76,19 @@ namespace BacktestingSoftware
             }
 
             string firstLine = reader.ReadLine();
+                string[] firstLineValues = firstLine.Split(splitter);
             if (!String.IsNullOrWhiteSpace(header))
             {
-                string[] firstLineValues = firstLine.Split(splitter);
                 if (firstLineValues[indices[1]].Contains("PM") || firstLineValues[indices[1]].Contains("AM"))
                 {
                     isEsignal11DateTimeFormat = true;
+                }
+                if(firstLineValues[indices[2]].Count(c => c == ',') > firstLineValues[indices[2]].Count(c => c == '.') ||
+                   firstLineValues[indices[3]].Count(c => c == ',') > firstLineValues[indices[3]].Count(c => c == '.') ||
+                   firstLineValues[indices[4]].Count(c => c == ',') > firstLineValues[indices[4]].Count(c => c == '.') ||
+                   firstLineValues[indices[5]].Count(c => c == ',') > firstLineValues[indices[5]].Count(c => c == '.'))
+                {
+                    isCommaDecimalPoint = true;
                 }
             }
 
@@ -89,12 +97,12 @@ namespace BacktestingSoftware
                    select line.Split(splitter)
                        into fields
                        let timeStamp = indices[2] == -100 ? new DateTime() : parseBarDateTime(fields[indices[0]], fields[indices[1]], isEsignal11DateTimeFormat)
-                       let open = indices[2] == -100 ? 0 : decimal.Parse(fields[indices[2]], CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
-                       let high = indices[3] == -100 ? 0 : decimal.Parse(fields[indices[3]], CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
-                       let low = indices[4] == -100 ? 0 : decimal.Parse(fields[indices[4]], CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
-                       let close = indices[5] == -100 ? 0 : decimal.Parse(fields[indices[5]], CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
+                       let open = indices[2] == -100 ? -1m : fields[indices[2]] == string.Empty ? -1 : decimal.Parse(fields[indices[2]], isCommaDecimalPoint ? new CultureInfo("fr-FR") : CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
+                       let high = indices[3] == -100 ? -1m : fields[indices[2]] == string.Empty ? -1 : decimal.Parse(fields[indices[3]], isCommaDecimalPoint ? new CultureInfo("fr-FR") : CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
+                       let low = indices[4] == -100 ? -1m : fields[indices[2]] == string.Empty ? -1 : decimal.Parse(fields[indices[4]], isCommaDecimalPoint ? new CultureInfo("fr-FR") : CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
+                       let close = indices[5] == -100 ? -1m : fields[indices[2]] == string.Empty ? -1 : decimal.Parse(fields[indices[5]], isCommaDecimalPoint ? new CultureInfo("fr-FR") : CultureInfo.InvariantCulture) / (isFullFuturePriceData ? innerValue : 1)
                        let volume = indices[6] == -100 ? 0 : long.Parse(fields[indices[6]], CultureInfo.InvariantCulture)
-                       where timeStamp.Date >= startDate.Date && timeStamp.Date <= endDate.Date
+                       where timeStamp.Date >= startDate.Date && timeStamp.Date <= endDate.Date && open > -1m && high > -1m && low > -1m && close > -1m
                        select new Tuple<DateTime, decimal, decimal, decimal, decimal, long>(timeStamp, open, high, low, close, volume);
         }
 
@@ -118,7 +126,13 @@ namespace BacktestingSoftware
 
             DateTime timeOfBar = DateTime.MinValue;
 
-            timeOfBar = timeOfBar.AddYears(int.Parse(dateValues[2]) - 1 + (isDataFromESignal11 || !isUSDateFormat ? 0 : 2000));
+            timeOfBar = timeOfBar.AddYears(int.Parse(dateValues[2]) - 1);
+            
+            if (timeOfBar.Year <= 2000)
+            {
+                timeOfBar = timeOfBar.AddYears(2000);
+            }
+
             timeOfBar = timeOfBar.AddMonths(int.Parse(dateValues[isUSDateFormat ? 0 : 1]) - 1);
             timeOfBar = timeOfBar.AddDays(int.Parse(dateValues[isUSDateFormat ? 1 : 0]) - 1);
 
